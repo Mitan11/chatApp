808b1f0b08582165408cb6a91388aab35c7c281c
mitan11 <mitantank00@gmail.com>
2025-06-08 18:16:21 +0530

    Added local dependency for chatapp in both backend and frontend, updated axios baseURL for production mode, and adjusted socket URL handling in useAuthStore.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,15 @@ import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { app , server } from './lib/socket.js'
+import { app, server } from './lib/socket.js'
+import path from 'path'
 
 dotenv.config()
 
 // port
 const PORT = process.env.PORT || 8080;
+
+const __dirname = path().resolve()
 
 // middleware
 app.use(express.json({ limit: '50mb' }))
@@ -18,7 +21,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
 app.use(cors({
     origin: 'http://localhost:5173',
-    credentials : true
+    credentials: true
 }))
 // 
 app.get("/", (req, res) => {
@@ -28,6 +31,13 @@ app.get("/", (req, res) => {
 // 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.MODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.send(path.join(__dirname, "../../frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, (error) => {
     if (!error) {
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 import { useChatStore } from './useChatStore';
 import { io } from 'socket.io-client'
 
-const BASE_URL = import.meta.env.VITE_PUBLIC_SOCKET_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_PUBLIC_SOCKET_URL : "/"
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
