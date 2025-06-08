27edea29d26f84b40eda55b1a7f60b2cc54915dc
mitan11 <mitantank00@gmail.com>
2025-06-08 18:28:21 +0530

    Refactor backend index.js to remove unused path and fileURLToPath imports, and streamline CORS configuration. Update frontend axios and useAuthStore to consistently use VITE_PUBLIC_BACKEND_URL and VITE_PUBLIC_SOCKET_URL for base URLs.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,17 +5,12 @@ import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { app, server } from './lib/socket.js'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { app , server } from './lib/socket.js'
 
 dotenv.config()
 
 // port
 const PORT = process.env.PORT || 8080;
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 // middleware
 app.use(express.json({ limit: '50mb' }))
@@ -23,7 +18,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
 app.use(cors({
     origin: 'http://localhost:5173',
-    credentials: true
+    credentials : true
 }))
 // 
 app.get("/", (req, res) => {
@@ -33,13 +28,6 @@ app.get("/", (req, res) => {
 // 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
-
-if (process.env.MODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"))
-    })
-}
 
 server.listen(PORT, (error) => {
     if (!error) {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosObject = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_PUBLIC_BACKEND_URL : "/api",
+    baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
     withCredentials: true
 }) 
