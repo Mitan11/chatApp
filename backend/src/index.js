5ec027a5fd8158ae3cfee740719b7f6fedb044b2
mitan11 <mitantank00@gmail.com>
2025-06-08 18:24:46 +0530

    Refactor index.js to correctly resolve __dirname using fileURLToPath and update static file serving to use sendFile for improved compatibility.

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,13 +7,15 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { app, server } from './lib/socket.js'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 
 // port
 const PORT = process.env.PORT || 8080;
 
-const __dirname = path().resolve()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // middleware
 app.use(express.json({ limit: '50mb' }))
@@ -35,7 +37,7 @@ app.use("/api/messages", messageRoutes)
 if (process.env.MODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "../../frontend/dist")))
     app.get("*", (req, res) => {
-        res.send(path.join(__dirname, "../../frontend", "dist", "index.html"))
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"))
     })
 }
 
